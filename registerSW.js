02a83e0269547b9313aeee5ebb8be6e47acadae1
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2025-12-29 07:49:58 +0000

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/elfas-learn-english-magic/sw.js', { scope: '/elfas-learn-english-magic/' })})}
